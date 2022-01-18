b6d2ffb060b7831a46c58baeb86742dc527ff8fd
sangulo <sangulo@paradigmadigital.com>
2022-01-18 13:21:27 +0100

    images to base 64

--- rust/base64img/src/main.rs
+++ rust/base64img/src/main.rs
@@ -1,7 +1,7 @@
 extern crate image_base64;
 use std::fs;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{prelude::*, ErrorKind};
 
 fn main() -> std::io::Result<()> {
     for file in fs::read_dir("/home/sangulo/Imágenes/").unwrap() {
@@ -13,10 +13,20 @@ fn main() -> std::io::Result<()> {
                     if res.ends_with(".png") {
                         let image_path = "/home/sangulo/Imágenes/".to_string() + &res.to_string();
                         let image_path_output =
-                            "/home/sangulo/Imágenes/b64/".to_string() + &res.to_string() + "_b64";
+                            "/home/sangulo/Imágenes/b65/".to_string() + &res.to_string() + "_b64";
                         let base64 = image_base64::to_base64(image_path.as_str());
-                        let mut file = File::create(image_path_output)?;
-                        file.write_all(base64.as_bytes())?;
+                        match File::create(&image_path_output) {
+                            Ok(mut fi) => {
+                                fi.write_all(base64.as_bytes())?;
+                            }
+                            Err(e) => {
+                                if e.kind() == ErrorKind::NotFound {
+                                    fs::create_dir("/home/sangulo/Imágenes/b65")?;
+                                    let mut fi = File::create(&image_path_output)?;
+                                    fi.write_all(base64.as_bytes())?;
+                                }
+                            }
+                        };
                     }
                 }
                 _ => {}
